8514e50baf0ca368bc1a64a62258b8c3e43e31b3
jordanjoelson <jjoelson007@gmail.com>
2024-11-16 17:24:23 -0600

    ACTUAL FRAMEWORK

--- electron/webpack.rules.js
+++ electron/webpack.rules.js
@@ -1,22 +1,24 @@
+const path = require("path");
+
 module.exports = [
   // Add support for native node modules
   {
     // We're specifying native_modules in the test because the asset relocator loader generates a
     // "fake" .node file which is really a cjs file.
     test: /native_modules[/\\].+\.node$/,
-    use: 'node-loader',
+    use: "node-loader",
   },
   {
     test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
     parser: { amd: false },
     use: {
-      loader: '@vercel/webpack-asset-relocator-loader',
+      loader: "@vercel/webpack-asset-relocator-loader",
       options: {
-        outputAssetBase: 'native_modules',
+        outputAssetBase: "native_modules",
       },
     },
   },
-  { 
+  {
     test: /\.jsx?$/,
     use: {
       loader: "babel-loader",
@@ -26,6 +28,13 @@ module.exports = [
       },
     },
   },
+  {
+    // loads .css files
+    test: /\.css$/,
+    include: [path.resolve(__dirname, "app/src")],
+    use: ["style-loader", "css-loader", "postcss-loader"],
+  },
+
   // Put your webpack loader rules in this array.  This is where you would put
   // your ts-loader configuration for instance:
   /**
