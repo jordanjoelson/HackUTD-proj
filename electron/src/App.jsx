1a8879bee4272e388755f36b1ea133eddca619d2
jordanjoelson <jjoelson007@gmail.com>
2024-11-17 08:34:19 -0600

    routing

--- electron/src/App.jsx
+++ electron/src/App.jsx
@@ -1,12 +1,10 @@
-// src/App.jsx
 import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";  // Use HashRouter for Electron
 import Home from "./pages/home.jsx";
 import Dashboard from "./pages/dashboard.jsx";
-import Navbar from './components/navbar.jsx'; // Ensure 'Navbar' is capitalized
+import Navbar from './components/Navbar.jsx'; // Ensure 'Navbar' is capitalized
 import Bolt from './pages/Bolt.jsx'; // Import Bolt.jsx
-import ProductCatalog from './pages/productCatalog.jsx'; // Import Bolt.jsx
-
+import ProductCatalog from './pages/productCatalog.jsx'; // Import ProductCatalog.jsx
 
 export default function App() {
   return (
@@ -17,8 +15,7 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/bolt" element={<Bolt />} /> {/* Add Bolt route */}
-          <Route path="/product-catalog" element={<ProductCatalog />} /> 
-
+          <Route path="/product-catalog" element={<ProductCatalog />} /> {/* Add ProductCatalog route */}
         </Routes>
       </div>
     </Router>
